ef45928bc45b3e786c7c5c21deaaeae70b60bef5
Ramdhas <ramdhas@MacBook-Pro-som-tillhor-Ramdhas.local>
2023-09-25 19:04:20 +0200

    Initial commit with basic layout and design.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=4e1ab24965d5dd41d94b317adb7e7670"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'> 
+        <div class="top">
+          <div class="location">
+            <p>Chennai</p>
+          </div>
+          <div class="temp">
+            <h1>37°C</h1>
+          </div>
+          <div class="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div class="bottom">
+          <div class="feels">
+            <p>40°C</p>
+            <p class='bold'> Feels Like</p>
+          </div>
+          <div class="humidity">
+            <p>50%</p>
+            <p class='bold'>Humidity</p>
+          </div>
+          <div class="wind">
+            <p>12mph</p>
+            <p class='bold'>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
